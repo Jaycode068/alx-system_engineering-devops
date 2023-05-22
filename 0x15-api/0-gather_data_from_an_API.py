a83807a53a7d882d50e01ab09e8b53595cbb109c
Jaycode068 <ajayijohnson68@yahoo.com>
2023-05-22 12:29:12 -0700

    GHathering data from an API

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python3
-
+"""
+    Using REST API, for a given employee ID, returns information about his/her
+    TODO list progress. Calls to data from the jsonplaceholder website. The
+    script must display on the standard output the employee TODO list progress
+"""
 if __name__ == "__main__":
     """ Second and N next lines display the title of completed tasks """
 
@@ -13,17 +17,17 @@ if __name__ == "__main__":
     EMPLOYEE_NAME = user_response.json().get("name")
 
     todo_list = requests.get(f"{link}/{EMPLOYEE_ID}/todos").json()
-    total = len(todo_list)
-    completed = [
+    TOTAL_NUMBER_OF_TASKS = len(todo_list)
+    NUMBER_OF_DONE_TASKS = [
             task.get('title')
             for task in todo_list
-            if task.get('completed') is True
+            if task.get('NUMBER_OF_DONE_TASKS') is True
         ]
 
     print("Employee {} is done with tasks({}/{})".format(
         EMPLOYEE_NAME,
-        len(completed),
-        total))
+        len(NUMBER_OF_DONE_TASKS),
+        TOTAL_NUMBER_OF_TASKS))
 
-    for task in completed:
+    for task in NUMBER_OF_DONE_TASKS:
         print(f"\t {task}")
