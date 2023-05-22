603ce21f70d13ec1f041705b316b7bc6030f1341
Jaycode068 <ajayijohnson68@yahoo.com>
2023-05-22 12:20:43 -0700

    Gathering data from an API

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,14 +1,29 @@
 #!/usr/bin/python3
-"""Returns to-do list information for a given employee ID."""
-import requests
-import sys
 
 if __name__ == "__main__":
-    url = "https://jsonplaceholder.typicode.com/"
-    user = requests.get(url + "users/{}".format(sys.argv[1])).json()
-    todos = requests.get(url + "todos", params={"userId": sys.argv[1]}).json()
+    """ Second and N next lines display the title of completed tasks """
 
-    completed = [t.get("title") for t in todos if t.get("completed") is True]
-    print("Employee {} is done with tasks({}/{}):".format(
-        user.get("name"), len(completed), len(todos)))
-    [print("\t {}".format(c)) for c in completed]
+    import requests
+    import sys
+
+    EMPLOYEE_ID = sys.argv[1]
+
+    link = "https://jsonplaceholder.typicode.com/users"
+    user_response = requests.get(f"{link}/{EMPLOYEE_ID}")
+    EMPLOYEE_NAME = user_response.json().get("name")
+
+    todo_list = requests.get(f"{link}/{EMPLOYEE_ID}/todos").json()
+    total = len(todo_list)
+    completed = [
+            task.get('title')
+            for task in todo_list
+            if task.get('completed') is True
+        ]
+
+    print("Employee {} is done with tasks({}/{})".format(
+        EMPLOYEE_NAME,
+        len(completed),
+        total))
+
+    for task in completed:
+        print(f"\t {task}")
